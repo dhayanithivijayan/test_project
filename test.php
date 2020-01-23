c09abe841b626d5bbaa0e0b853c45f18b0bfa595
dhayanithivijayan <dhayanithivijay@gmail.com>
2020-01-24 04:53:32 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', 0);
+$total=10;
+?>
+<html>
+<head>
+<script type="text/javascript">
+  adroll_conversion_value = <?php echo $total; ?>;
+  adroll_currency = "USD";
+  </script>
+</head>
+<body>
+<!--	<table class="table" style="vertical-align: middle; text-align: center;">
+	  <thead class="thead-dark">
+		<tr>
+			<th scope="col">Total</th>
+		  
+		</tr>
+	  </thead>
+	  <tbody>		
+		<tr>
+		  <th scope="row"></th>
+			<td><?php echo $total; ?></td>		  
+		</tr>		
+	  </tbody>
+	</table>	-->
+</body>
+</html>		
